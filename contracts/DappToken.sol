12ff4fc600588e31c72b9df20ba57f4ed4b39ef9
james-truong <james.truong@hotmail.com>
2021-05-31 15:20:06 -0400

    added symbol,name

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,9 +2,17 @@ pragma solidity ^0.5.16;
 
 contract DappToken {
 
+    string  public name = "DApp Token";
+    string  public symbol = "DAPP";
+    string  public standard = "DApp Token v1.0";
     uint256 public totalSupply;
 
-    constructor () public {
-        totalSupply = 1000000;
+    mapping(address => uint256) public balanceOf;
+
+    constructor (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        // allocate initial supply somewhere... starting point of where tokens are held
     }
+
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,6 +1,26 @@
 var DappToken = artifacts.require("./DappToken.sol");
 
 contract("DappToken", function (accounts) {
+  let tokenInstance;
+
+  it("initializes the contract with the correct values", function () {
+    return DappToken.deployed()
+      .then(function (instance) {
+        tokenInstance = instance;
+        return tokenInstance.name();
+      })
+      .then(function (name) {
+        assert.equal(name, "DApp Token", "has the correct name");
+        return tokenInstance.symbol();
+      })
+      .then(function (symbol) {
+        assert.equal(symbol, "DAPP", "has the correct symbol");
+        return tokenInstance.standard();
+      })
+      .then(function (standard) {
+        assert.equal(standard, "DApp Token v1.0", "has the correct standard");
+      });
+  });
   it("allocates the initial supply upon deployment", function () {
     return DappToken.deployed()
       .then(function (instance) {
@@ -12,6 +32,14 @@ contract("DappToken", function (accounts) {
           totalSupply.toNumber(),
           1000000,
           "sets the total supply to 1,000,000"
+        );
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then(function (adminBalance) {
+        assert.equal(
+          adminBalance.toNumber(),
+          1000000,
+          "it allocates initial supply to admin"
         );
       });
   });
