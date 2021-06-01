e238b662dd2da63598b432a410c209673aeb2094
james-truong <james.truong@hotmail.com>
2021-06-01 15:26:22 -0400

    added transfer function to token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -7,12 +7,29 @@ contract DappToken {
     string  public standard = "DApp Token v1.0";
     uint256 public totalSupply;
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
 
     constructor (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
         // allocate initial supply somewhere... starting point of where tokens are held
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 
 }
