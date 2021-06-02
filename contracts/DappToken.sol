151f2c36af8fa4e749ede6621fa7ca562fb5b9b9
james-truong <james.truong@hotmail.com>
2021-06-02 14:14:43 -0400

    added approval function

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -13,7 +13,15 @@ contract DappToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -31,5 +39,14 @@ contract DappToken {
 
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
 
 }
