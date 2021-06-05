55cdc0519cb28cc712154b12e94be70c865f64d2
james-truong <james.truong@hotmail.com>
2021-06-05 15:40:37 -0400

    added tokenSale skeleton

---
+++ test/DappTokenSale.js
@@ -0,0 +1,27 @@
+const DappToken = artifacts.require("./DappToken.sol");
+const DappTokenSale = artifacts.require("./DappTokenSale.sol");
+
+contract("DappTokenSale", function (accounts) {
+  let tokenInstance;
+  let tokenSaleInstance;
+  let tokenPrice = 1000000000000000; // in wei
+
+  it("initializes the contract with the correct values", function () {
+    return DappTokenSale.deployed()
+      .then(function (instance) {
+        tokenSaleInstance = instance;
+        return tokenSaleInstance.address;
+      })
+      .then(function (address) {
+        assert.notEqual(address, 0x0, "has contract address");
+        return tokenSaleInstance.tokenContract();
+      })
+      .then(function (address) {
+        assert.notEqual(address, 0x0, "has token contract address");
+        return tokenSaleInstance.tokenPrice();
+      })
+      .then(function (price) {
+        assert.equal(price, tokenPrice, "token price is correct");
+      });
+  });
+});
